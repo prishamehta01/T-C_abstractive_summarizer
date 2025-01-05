3e18d9991dd5e86932539a0a8876b53e7471279d
Payal Lenka <payalm.lenka@gmail.com>
2025-01-05 10:35:41 +0530

    Update app.py
    Made changes on port

--- app.py
+++ app.py
@@ -126,4 +126,7 @@ def summarize():
 CORS(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
+    
